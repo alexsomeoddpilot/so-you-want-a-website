88cf7e02cbef59958d4e23e44c8858de4a338fd5
alexsomeoddpilot <alex@someoddpilot.com>
2014-08-28 17:02:37 -0500

    Fixing mailer formatting

--- api/services/Mailer.js
+++ api/services/Mailer.js
@@ -2,25 +2,25 @@ var nodemailer = require('nodemailer');
 
 // create reusable transport method (opens pool of SMTP connections)
 var smtpTransport = nodemailer.createTransport('SMTP', {
-    service: 'Mandrill',
-    auth: {
-        user: sails.config.mandrill.user,
-        pass: sails.config.mandrill.pass
-    }
+  service: 'Mandrill',
+  auth: {
+    user: process.env.MANDRILL_USERNAME,
+    pass: process.env.MANDRILL_APIKEY
+  }
 });
 
 module.exports = {
-    mail: function(message, callback) {
-        // send mail with defined transport object
-        smtpTransport.sendMail(message, function (error, response) {
-            if (error) {
-                sails.log.verbose(error);
-                callback();
-                return;
-            }
+  mail: function(message, callback) {
+    // send mail with defined transport object
+    smtpTransport.sendMail(message, function (error, response) {
+      if (error) {
+        sails.log.verbose(error);
+        callback();
+        return;
+      }
 
-            sails.log.verbose('Message sent: ' + response.message);
-            callback();
-        });
-    }
+      sails.log.verbose('Message sent: ' + response.message);
+      callback();
+    });
+  }
 };
